1c84e793882382a50e54ac40a8f1fe599c26f1df
nm781980682 <781980682@qq.com>
2018-08-09 17:29:35 +0800

    jq

---
+++ zeeeey/zyee.js
@@ -0,0 +1,30 @@
+// $('.mod-tab .tab').on('click', function () {
+//   $(this).addClass('active')
+//     .siblings()
+//     .removeClass('active')
+
+//   $(this).parents('.mod-tab')
+//     .find('.panel')
+//     .eq($(this).index())
+//     .addClass('active')
+//     .siblings()
+//     .removeClass('active')
+// })
+
+
+document.querySelectorAll('.mod-tab .tab').forEach(function (node) {
+  node.addEventListener('click', function () {
+    var index
+    this.parentElement.querySelectorAll('.tab').forEach(function (tab, idx) {
+      tab.classList.remove('active')
+      if (node === tab) {
+        index = idx
+      }
+    })
+    this.classList.add('active')
+    this.parentElement.nextElementSibling.querySelectorAll('.panel').forEach(function (panel) {
+      panel.classList.remove('active')
+    })
+    this.parentElement.nextElementSibling.querySelectorAll('.panel')[index].classList.add('active')
+  })
+})
