ec798284ae2b94b437efa33e51b6f0dd5361c233
nm781980682 <781980682@qq.com>
2018-09-08 13:22:10 +0800

    123

--- douban/index.js
+++ douban/index.js
@@ -271,6 +271,29 @@ var search = {
   }
 };
 
-
+var app = {
+  init: function() {
+    this.$tabs = $("footer>div");
+    this.$panels = $("section");
+    this.bind();
+    top250.init();
+    usBox.init();
+    search.init();
+  },
+  bind: function() {
+    var _this = this;
+    this.$tabs.on("click", function() {
+      $(this)
+        .addClass("active")
+        .siblings()
+        .removeClass("active");
+      _this.$panels
+        .eq($(this).index())
+        .fadeIn()
+        .siblings()
+        .hide();
+    });
+  }
+};
 
 app.init();
